65ab16a9be9dea479d30a00d2927e8f5fbdde5aa
U-KK\KK <C:\Users\KK\AppData\Roaming\The Bat! Pwd>
2020-03-28 13:14:15 +0100

    Implementation of showing the status of Ping

--- Service.go
+++ Service.go
@@ -8,28 +8,31 @@ import (
 	"github.com/tatsushid/go-fastping"
 )
 
-func ping(addr string) error {
+func ping(addr string) (bool, error) {
 
 	p := fastping.NewPinger()
 	ra, err := net.ResolveIPAddr("ip4:icmp", addr /*inputIP.Text*/)
+	var recieved bool = false
 
 	if err != nil {
 		log.Println(err)
-		return err
+		return recieved, err
 	}
 
 	p.AddIPAddr(ra)
 	p.OnRecv = func(addr *net.IPAddr, rtt time.Duration) {
+		recieved = true
 		log.Printf("IP Addr: %s receive, RTT: %v\n", addr.String(), rtt)
 	}
 
 	p.OnIdle = func() {
-		log.Println("finish")
+		log.Println("Ping job finished")
 	}
 
 	err = p.Run()
 	if err != nil {
 		log.Println(err)
 	}
-	return nil
+
+	return recieved, nil
 }
