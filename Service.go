07a783bbefffc54d86f7e2d3e541b0626c3dc79e
Johannes Krainer <j.krainer@posteo.de>
2020-03-30 19:00:27 +0200

    First Implementation of the pcap Part

--- Service.go
+++ Service.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"encoding/hex"
 	"fmt"
 	"io"
 	"log"
@@ -14,13 +15,20 @@ import (
 
 	"os/exec"
 
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
 	"github.com/tatsushid/go-fastping"
 )
 
 type config struct {
-	defaultIface net.IP // the interface to reach default gateway
-	pcapIface    pcap.Interface
+	defaultIface  net.IP // the interface to reach default gateway
+	pcapIface     pcap.Interface
+	pcapHandle    *pcap.Handle
+	pcapPortUDP   uint
+	VPNGateway    string // TODO from 'arp -a' to net.HardwareAddr
+	VPNHostTarget net.IP
+	VPNHostSource net.IP
 }
 
 var currentConfig config
@@ -114,7 +122,93 @@ func initService() {
 		}
 	}
 
-	fmt.Println(currentConfig.pcapIface.Description)
+	fmt.Println("Using '", currentConfig.pcapIface.Description, "' to Capture Broadcast Packets")
+
+	handle, err := pcap.OpenLive(currentConfig.pcapIface.Name, 65536, false, pcap.BlockForever)
+	if err != nil {
+		panic(err)
+	}
+
+	currentConfig.pcapHandle = handle
+	currentConfig.VPNGateway = "c8:0e:14:30:fd:d7" // TODO from arp
+}
+
+func startServicePcap() {
+
+	fmt.Println("Configuring Broadcast Packet Capture on UDP Port", currentConfig.pcapPortUDP)
+
+	filter := fmt.Sprintf("host %s and ip broadcast and udp port %d",
+		currentConfig.defaultIface.String(),
+		currentConfig.pcapPortUDP)
+
+	fmt.Println("Setting Filter '" + filter + "'")
+
+	if err := currentConfig.pcapHandle.SetBPFFilter(filter); err != nil {
+		panic(err)
+	}
+
+	packetSource := gopacket.NewPacketSource(currentConfig.pcapHandle, currentConfig.pcapHandle.LinkType())
+	for packet := range packetSource.Packets() {
+
+		var thisSrcMAC net.HardwareAddr
+		var thisDstMAC net.HardwareAddr
+		var thisSrcIP net.IP
+		var thisDstIP net.IP
+		var thisSrcPort layers.UDPPort
+		var thisDstPort layers.UDPPort
+
+		ethLayer := packet.Layer(layers.LayerTypeEthernet)
+		if ethLayer == nil {
+			panic("Layer Ethernet is nil")
+		}
+		eth, _ := ethLayer.(*layers.Ethernet)
+		thisSrcMAC = eth.SrcMAC
+		thisDstMAC = eth.DstMAC
+
+		ip4Layer := packet.Layer(layers.LayerTypeIPv4)
+		if ip4Layer == nil {
+			panic("Layer IPv4 is nil")
+		}
+		ip4, _ := ip4Layer.(*layers.IPv4)
+		thisSrcIP = ip4.SrcIP
+		thisDstIP = ip4.DstIP
+
+		udpLayer := packet.Layer(layers.LayerTypeUDP)
+		if udpLayer == nil {
+			panic("Layer UDP is nil")
+		}
+		udp, _ := udpLayer.(*layers.UDP)
+		thisSrcPort = udp.SrcPort
+		thisDstPort = udp.DstPort
+
+		fmt.Printf("%s %d:%d -> %s %d:%d\n", thisSrcMAC, thisSrcIP, thisSrcPort, thisDstMAC, thisDstIP, thisDstPort)
+
+		// redirect paket
+
+		gwMACDecoded, err := hex.DecodeString(strings.ReplaceAll(currentConfig.VPNGateway, ":", ""))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		eth.DstMAC = gwMACDecoded
+		ip4.DstIP = currentConfig.VPNHostTarget
+
+		fmt.Printf("Redirecting...\n")
+		fmt.Printf("%s %d:%d -> %s %d:%d\n", thisSrcMAC, thisSrcIP, thisSrcPort, currentConfig.VPNGateway, ip4.DstIP, thisDstPort)
+
+		buf := gopacket.NewSerializeBuffer()
+		opts := gopacket.SerializeOptions{}
+
+		gopacket.SerializeLayers(buf, opts,
+			eth,
+			ip4,
+			udp,
+			gopacket.Payload(packet.ApplicationLayer().Payload()))
+
+		packetData := buf.Bytes()
+
+		currentConfig.pcapHandle.WritePacketData(packetData)
+	}
 }
 
 func ping(addr string) (bool, error) {
--- VPNTunneler.go
+++ VPNTunneler.go
@@ -1,6 +1,15 @@
 package main
 
+import "net"
+
 func main() {
+
 	initService()
 	//InitGUI()
+
+	// TODO from gui
+	currentConfig.pcapPortUDP = 6112
+	currentConfig.VPNHostTarget = net.IP{192, 168, 1, 2}
+
+	startServicePcap() // TODO from gui, new thread
 }
