c4e3cfa92214c16ab794c360c55082b3a3fab56c
KingKeule <62748655+KingKeule@users.noreply.github.com>
2020-05-11 16:32:11 +0200

    Update GUI: add menü and protocol type selection, Add global confguration

--- GUI.go
+++ GUI.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"strconv"
 
 	"fyne.io/fyne"
 	"fyne.io/fyne/app"
@@ -10,7 +11,8 @@ import (
 )
 
 var screenWidth = 280
-var screenHight = 400
+var screenHight = 400 // not really used because the minimum height is desired
+var containerHight = 70
 
 //InitGUI design the GUI of the appp
 func InitGUI() {
@@ -22,19 +24,32 @@ func InitGUI() {
 	w := app.NewWindow("VPN packet tunneler")
 
 	// Set a sane default for the window size.
-	w.Resize(fyne.NewSize(screenWidth, screenHight))
+	// w.Resize(fyne.NewSize(screenWidth, screenHight))
 
 	// ---------------- Container Configuration ----------------
+	// set default values for IP and Port from global config
 	inputIP := widget.NewEntry()
-	inputIP.SetPlaceHolder("192.168.XXX.XXX")
+	inputIP.SetPlaceHolder(dstIP.String())
 
 	inputPort := widget.NewEntry()
-	inputPort.SetPlaceHolder("6112")
+	inputPort.SetPlaceHolder(strconv.Itoa(dstPort))
+
+	labelProtocolTpye := widget.NewLabelWithStyle("Protocol type", fyne.TextAlignLeading, fyne.TextStyle{Bold: true})
+	selectProtocolTpye := widget.NewSelect([]string{"UDP", "TCP"}, func(selected string) {
+		switch selected {
+		case "UDP":
+			protocoltpye = "UDP"
+		case "TCP":
+			protocoltpye = "TCP"
+		}
+	})
+	containerProtocolTpye := fyne.NewContainerWithLayout(layout.NewGridLayout(2),
+		labelProtocolTpye, selectProtocolTpye)
 
 	widgetGroupConf := widget.NewGroup("Configuration", &widget.Form{
 		// Items: []*widget.FormItem{{"IP of Server", inputIP}, {"Port of Server", inputPort}, {"", widget.NewLabel("")}}, // ToDo: Remove when spacer is working
 		Items: []*widget.FormItem{{"IP of Server", inputIP}, {"Port of Server", inputPort}},
-	})
+	}, containerProtocolTpye)
 
 	// ---------------- Container Ping ----------------
 	widgetPingStatus := widget.NewLabelWithStyle("", fyne.TextAlignCenter, fyne.TextStyle{Bold: true})
@@ -44,10 +59,8 @@ func InitGUI() {
 		recieved, err := ping(inputIP.Text)
 		if err != nil || !recieved {
 			widgetPingStatus.SetText("NOK")
-			w.Show()
 		} else {
 			widgetPingStatus.SetText("OK")
-			w.Show()
 		}
 	})
 
@@ -63,25 +76,47 @@ func InitGUI() {
 		log.Println("Start tunneling service")
 		//widgetTunnelServiceStat = widget.NewLabel("Running") //ToDo : Change Status of widget
 	})
-	//buttonTunnelServiceStat.Resize(fyne.NewSize(2 00, 200)) // ToDo: Resize not working. Change size -> smaler
 
 	widgetGroupTunnelService := widget.NewGroup("Tunnelling Service", fyne.NewContainerWithLayout(layout.NewGridLayout(2),
 		buttonTunnelServiceStat, widgetTunnelServiceStat))
 
-	// ToDo: Spacer espcially the size of the spacer are not really working
-	// spacer := layout.NewSpacer()
-	// spacer.Resize(fyne.NewSize(500, 500))
-
 	// ---------------- Container complete ----------------
-	// Add all the buttons in to a three column grid layout inside a container.
 	containerAll := fyne.NewContainerWithLayout(layout.NewGridLayout(1),
 		widgetGroupConf,
-		// spacer,
-		widgetGroupPing,
-		widgetGroupTunnelService)
-	// rect)
+		//FixedGridLayout required cause GridLayout divides the space equally for all cells
+		fyne.NewContainerWithLayout(layout.NewFixedGridLayout(fyne.NewSize(screenWidth, containerHight)),
+			widgetGroupPing,
+			widgetGroupTunnelService))
 
 	w.SetContent(containerAll)
+
+	w.SetMainMenu(fyne.NewMainMenu(
+		fyne.NewMenu("Tool",
+			fyne.NewMenuItem("Reset configuration", func() {
+				setDefaultConf()
+				//TODO find an better way for update the variables
+				inputIP.SetText(strconv.Itoa(dstPort))
+				inputPort.SetText(strconv.Itoa(dstPort))
+				//TODO not possible to go back to default choice
+				// selectProtocolTpye.SetSelected("Select one")
+				widgetPingStatus.SetText("")
+			}),
+			fyne.NewMenuItem("Import configuration", func() {})),
+		fyne.NewMenu("Preload Configuration",
+			fyne.NewMenuItem("Warcraft 3", func() {
+				setWar3Conf()
+				//TODO find an better way for update the variables
+				inputPort.SetText(strconv.Itoa(dstPort))
+				selectProtocolTpye.SetSelected(protocoltpye)
+			})),
+		fyne.NewMenu("Help",
+			fyne.NewMenuItem("Show Log", func() {
+				//TODO Integrate Logviewer
+			}),
+			fyne.NewMenuItem("About", func() {
+				//TODO Integrate PopUp
+			}),
+		)))
 
 	// Show all of our set content and run the gui.
 	w.ShowAndRun()
