d8c53ea938c6587af8dd2be6131332010e245654
KingKeule <62748655+KingKeule@users.noreply.github.com>
2020-05-22 20:19:01 +0200

    Add graphical error messages by wrong input

--- GUI.go
+++ GUI.go
@@ -3,10 +3,12 @@ package main
 import (
 	"log"
 	"net"
+	"os/exec"
 	"strconv"
 
 	"fyne.io/fyne"
 	"fyne.io/fyne/app"
+	"fyne.io/fyne/dialog"
 	"fyne.io/fyne/layout"
 	"fyne.io/fyne/widget"
 )
@@ -15,14 +17,23 @@ var screenWidth = 280
 var screenHight = 400 // not really used because the minimum height is desired
 var containerHight = 70
 
+const version = "v0.9"
+const gitHubLink = "https://github.com/KingKeule/VPN-broadcast-tunneler"
+
 //InitGUI design the GUI of the appp
 func InitGUI() {
 
 	// Initialize our new fyne interface application.
 	app := app.New()
 
+	// set the theme for the app. Default is dark theme
+	//app.Settings().SetTheme(theme.LightTheme())
+
 	// Initialize our new fyne interface application.
-	w := app.NewWindow("  VPNubt v0.9")
+	w := app.NewWindow("  VPNubt " + version)
+
+	// indicates that closing this main window should exit the app
+	w.SetMaster()
 
 	// center the windows on the screen
 	w.CenterOnScreen()
@@ -59,8 +70,7 @@ func InitGUI() {
 
 	buttonPing := widget.NewButton("Ping Server", func() {
 		selecteddstIP := net.ParseIP(inputdstIP.Text)
-		if (selecteddstIP == nil) || (selecteddstIP.IsUnspecified()) {
-			log.Println("The IP address is not correct. Please set a valid IP adress.")
+		if !checkIPAdress(selecteddstIP, w) {
 		} else {
 			log.Println("Start pinging server (IP: " + selecteddstIP.String() + ")")
 			recieved, err := ping(selecteddstIP.String())
@@ -89,24 +99,20 @@ func InitGUI() {
 	buttonTunnelServiceStat := widget.NewButton("Start / Stop", func() {
 		port, err := strconv.Atoi(inputdstPort.Text)
 		selecteddstIP := net.ParseIP(inputdstIP.Text)
-		if (selecteddstIP == nil) || (selecteddstIP.IsUnspecified()) {
-			log.Println("The IP address is not correct. Please set a valid IP adress.")
-		} else if (err != nil) || (port < 1) || (port > 65535) {
-			log.Println("The UDP port is not correct. Please set a UDP valid port (1-65535).")
-		} else if selectNetDevice.Selected == "" {
-			log.Println("The selection of the network device is not set. Please select network device.")
+
+		if !checkIPAdress(selecteddstIP, w) {
+		} else if !checkPort(err, port, w) {
+		} else if !checkNetDevice(selectNetDevice.Selected, w) {
+		} else if !serviceRunning {
+			log.Println("Starting udp broadcast tunneling service")
+			widgetTunnelServiceStat.SetText("Running")
+			go capturePackets(stopThreadChannel, selectNetDevice.Selected, selecteddstIP, port)
+			serviceRunning = true
 		} else {
-			if !serviceRunning {
-				log.Println("Starting udp broadcast tunneling service")
-				widgetTunnelServiceStat.SetText("Running")
-				go capturePackets(stopThreadChannel, selectNetDevice.Selected, selecteddstIP, port)
-				serviceRunning = true
-			} else {
-				log.Println("Stopping udp broadcast tunneling service")
-				stopThreadChannel <- true // Send stop signal to channel.
-				widgetTunnelServiceStat.SetText("Stopped")
-				serviceRunning = false
-			}
+			log.Println("Stopping udp broadcast tunneling service")
+			stopThreadChannel <- true // Send stop signal to channel.
+			widgetTunnelServiceStat.SetText("Stopped")
+			serviceRunning = false
 		}
 	})
 
@@ -136,7 +142,7 @@ func InitGUI() {
 				widgetPingStatus.SetText("")
 				widgetTunnelServiceStat.SetText("")
 			})),
-		fyne.NewMenu("Preload Configuration",
+		fyne.NewMenu("Game selection",
 			fyne.NewMenuItem("Warcraft 3", func() {
 				w3Conf := getWar3Conf()
 				//TODO find an better way for update the variables and move menu ahead
@@ -147,10 +153,50 @@ func InitGUI() {
 				//TODO Integrate Logviewer
 			}),
 			fyne.NewMenuItem("About", func() {
-				//TODO Integrate PopUp
+				// windows command to open the browser with the given link
+				exec.Command("rundll32", "url.dll,FileProtocolHandler", gitHubLink).Start()
 			}),
 		)))
 
 	// Show all of our set content and run the gui.
 	w.ShowAndRun()
+}
+
+// checks whether the given string is a valid IP address
+// if not then an error message is displayed on the given window
+func checkIPAdress(ip net.IP, window fyne.Window) bool {
+	if (ip == nil) || (ip.IsUnspecified()) {
+		ipWarnText1 := "The entered IP address is not correct."
+		ipWarnText2 := "Please set a valid IP adress."
+		log.Println(ipWarnText1 + " " + ipWarnText2)
+		dialog.ShowInformation("", ipWarnText1+"\n"+ipWarnText2, window)
+		return false
+	}
+	return true
+}
+
+// checks whether the given string is a valid port
+// if not then an error message is displayed on the given window
+func checkPort(err error, port int, window fyne.Window) bool {
+	if (err != nil) || (port < 1) || (port > 65535) {
+		portWarnText1 := "The entered UDP port is not correct."
+		portWarnText2 := "Please set a UDP valid port (1-65535)."
+		log.Println(portWarnText1 + " " + portWarnText2)
+		dialog.ShowInformation("", portWarnText1+"\n"+portWarnText2, window)
+		return false
+	}
+	return true
+}
+
+// checks whether a network device is selected
+// if not then an error message is displayed on the given window
+func checkNetDevice(netDevice string, window fyne.Window) bool {
+	if netDevice == "" {
+		netDevWarnText1 := "The network device is not set."
+		netDevWarnText2 := "Please select network device."
+		log.Println(netDevWarnText1 + " " + netDevWarnText2)
+		dialog.ShowInformation("", netDevWarnText1+"\n"+netDevWarnText2, window)
+		return false
+	}
+	return true
 }
